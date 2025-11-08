a983e40cab86ea39bc6726dc55d7730eb9bd31fb
VidakGrujic <vidakspartanacgrujic@gmail.com>
2025-11-08 08:23:39 +0100

    Added exercise 2

---
+++ BI_Projects/DWH2_008/ddl/airq_reset_dwh2_008.sql
@@ -0,0 +1,5 @@
+-- -------------------------------
+-- 1) Assignment 1: create/reset dwh2_xxx schema per group
+-- -------------------------------
+DROP SCHEMA IF EXISTS dwh2_008 CASCADE;
+CREATE SCHEMA dwh2_008 AUTHORIZATION grp_008;
---
+++ BI_Projects/DWH2_008/ddl/airq_reset_stg2_008.sql
@@ -0,0 +1,5 @@
+-- -------------------------------
+-- 1) Assignment 1: create/reset stg2_xxx schema per group
+-- -------------------------------
+DROP SCHEMA IF EXISTS stg2_008 CASCADE;
+CREATE SCHEMA stg2_008 AUTHORIZATION grp_008;
